78ec354f615d5ffc4870692cfe17afc0da662d69
liliia baskakova <lbaskakova55@gmail.com>
2026-06-26 16:18:02 +0300

    Add files via upload

---
+++ app/params.py
@@ -0,0 +1,6 @@
+CATBOOST_HYPERPARAMETRS = {'iterations': 494, 'learning_rate': 0.3788119430934142, 'loss_function': 'MAE', 'depth': 2, 'l2_leaf_reg': 38.39899891347804, 'min_data_in_leaf': 57}
+LIGHTGBM_HYPERPARAMETRS = {'learning_rate': 0.05593308405682298, 'num_leaves': 49, 'max_depth': 5, 'n_estimators': 523, 'bagging_fraction': 0.7553386111732491, 'min_child_samples': 177, 'feature_fraction': 0.5700191961275134}
+
+FEATURES = ['program', 'education_level', 'place_type', 'course', 'absence_status', 'discipline', 'module',
+       'exam_type', 'grade_10', 'difficulty_avg_score', 'exam_type_prev', 'grade_prev', 'difficulty_prev',
+       'avg_grade_prev', 'min_prev', 'max_prev']
